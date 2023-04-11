9a5d16421cde31cdeb1a21fc4e6d788dd1bce91e
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-04-11 22:25:01 +0600

    figure out textField create textFieldShouldReturn func create textFieldShouldEndEditing func create textFieldDidEndEditing func

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -11,11 +11,34 @@ class WeatherViewController: UIViewController, UITextFieldDelegate  {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchTextField.delegate = self
         
     }
 
 
     @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+        
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.text = ""
         
     }
 }
